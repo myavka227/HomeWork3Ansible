491df6351d696c7da4188e0dca959058d6137f41
myavka227 <sofia.gorbacheva.ru@gmail.com>
2026-02-15 08:51:21 +0300

    update

--- library/nginx_port.py
+++ library/nginx_port.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 from ansible.module_utils.basic import AnsibleModule
+import subprocess
+import re
+
+CONF = "/etc/nginx/conf.d/files.conf"
 
 def main():
     module = AnsibleModule(
@@ -9,11 +13,28 @@ def main():
     )
 
     port = module.params['port']
+    changed = False
+
+    try:
+        with open(CONF, "r") as f:
+            content = f.read()
+    except FileNotFoundError:
+        module.fail_json(msg="nginx config not found")
+
+    new_content = re.sub(r"listen\s+\d+;", f"listen {port};", content)
+
+    if new_content != content:
+        with open(CONF, "w") as f:
+            f.write(new_content)
+        changed = True
+
+        subprocess.run(["systemctl", "reload", "nginx"], check=True)
 
     module.exit_json(
-        changed=True,
-        message=f"Nginx port set to {port}"
+        changed=changed,
+        message=f"Nginx configured on port {port}"
     )
 
 if __name__ == '__main__':
     main()
+
